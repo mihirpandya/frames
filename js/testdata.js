3b793113e3acde8c720deff1f17c4144e0507498
Mihir Pandya <mihir.m.pandya@gmail.com>
2012-08-16 11:13:19 +0800

    Modified file structure

---
+++ js/testdata.js
@@ -0,0 +1,38 @@
+{
+			"David Guetta": {
+							"Album": "Nothing But the Beat",
+							"Num Tracks": "32"
+							},
+			"Michael Jackson":{
+							"Album": "Bad",
+							"Num Tracks": "10"
+							},
+			"Skrillex":{
+							"Album": "Scary Monsters and nice Spirits",
+							"Num Tracks": "9"
+							},
+			"Sum 41":{
+							"Album": "Chuck",
+							"Num Tracks": "13"
+							},
+			"Seal":{
+							"Album": "Soul",
+							"Num Tracks": "12"
+							},
+			"Justin Timberlake":{
+							"Album": "What Goes Around... Comes Around",
+							"Num Tracks": "5"
+							},
+			"Red Hot Chilli Peppers":{
+							"Album": "Stadium Arcadium",
+							"Num Tracks": "28"
+							},
+			"Bruno Mars":{
+							"Album": "Doo-Wops and Hooligans",
+							"Num Tracks": "10"
+							},
+			"Jason Mraz":{
+							"Album": "We Sing. We Dance. We Steal Things.",
+							"Num Tracks": "12"
+							}
+			}
